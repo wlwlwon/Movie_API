06379965e33ff3290054280420c05c41b4c11cc6
wlwlwon <ryanwon@naver.com>
2022-05-25 02:15:22 +0900

    Spring Config 추가

---
+++ src/main/java/com/dodatabase/movie_backend/SpringConfig.java
@@ -0,0 +1,16 @@
+package com.dodatabase.movie_backend;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+//    private final DataSource dataSource;
+
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+}
