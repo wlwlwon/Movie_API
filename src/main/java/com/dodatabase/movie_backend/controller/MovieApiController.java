049eafe9e7bd596dfa10a385a1f983a464e0ad16
wlwlwon <ryanwon@naver.com>
2022-05-25 02:13:08 +0900

    Movie Controller 추가

---
+++ src/main/java/com/dodatabase/movie_backend/controller/MovieApiController.java
@@ -0,0 +1,17 @@
+package com.dodatabase.movie_backend.controller;
+
+import com.dodatabase.movie_backend.domain.Movie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class MovieApiController {
+
+//    @PostMapping("/create")
+//    public ResponseEntity<?> create(@RequestBody Movie movie){
+//    }
+}
---
+++ src/main/java/com/dodatabase/movie_backend/controller/MovieController.java
@@ -0,0 +1,7 @@
+package com.dodatabase.movie_backend.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MovieController {
+}
