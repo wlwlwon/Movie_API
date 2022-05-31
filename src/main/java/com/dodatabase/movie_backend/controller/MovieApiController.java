ca1a28e26b00d6732513beda16f066c58ea1545f
wlwlwon <ryanwon@naver.com>
2022-05-31 19:49:19 +0900

    movie api controller 수정

--- src/main/java/com/dodatabase/movie_backend/controller/MovieApiController.java
+++ src/main/java/com/dodatabase/movie_backend/controller/MovieApiController.java
@@ -3,21 +3,41 @@ package com.dodatabase.movie_backend.controller;
 import com.dodatabase.movie_backend.domain.Movie;
 import com.dodatabase.movie_backend.service.MovieApiService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.dodatabase.movie_backend.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+import java.util.Optional;
+
+@RestController
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class MovieApiController {
 
     private final MovieApiService movieApiService;
+    private final MovieService movieService;
 
-    @GetMapping("/api/v1/movies/{keyword}")
-    public Movie get(@PathVariable String keyword) {
-        return movieApiService.findByKeyword(keyword);
+    @PostMapping("/movies/{keyword}")
+    public ResponseEntity<Movie> get(@PathVariable String keyword) {
+
+        Movie result = null;
+        Optional<Movie> byTitle = movieService.findByTitle(keyword);
+
+        /**
+         * db에서 keyword조회후 존재하면 db에 있는 값 반환
+         */
+
+        if(byTitle.isEmpty()){
+            Movie byKeyword = movieApiService.findByKeyword(keyword);
+            result = movieService.create(byKeyword);
+        }else{
+            result = byTitle.get();
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/dodatabase/movie_backend/service/MovieApiService.java
+++ src/main/java/com/dodatabase/movie_backend/service/MovieApiService.java
@@ -2,20 +2,23 @@ package com.dodatabase.movie_backend.service;
 
 import com.dodatabase.movie_backend.domain.Movie;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class MovieApiService {
     
     private final MovieApiClient movieApiClient;
+    private final ModelMapper modelMapper;
 
     @Transactional(readOnly = true)
     public Movie findByKeyword(String keyword) {
-        return movieApiClient.requestMovie(keyword);
+
+        return modelMapper.map(movieApiClient.requestMovie2(keyword).getItems()[0], Movie.class);
     }
 
 }
